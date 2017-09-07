abff409554ad4439cebe2aea3da1d623e8f434c5
sucharitha93 <sucharitha93@gmail.com>
2017-09-07 22:32:10 +0530

    Initial commit

---
+++ app/src/main/java/amrita/cse/amuda/wifi_localization/AccessPoint.java
@@ -0,0 +1,72 @@
+package amrita.cse.amuda.wifi_localization;
+
+/**
+ * Created by SucharithaReddy on 9/5/2017.
+ */
+
+public class AccessPoint
+{
+    private String mac;
+    private float xCoordinate;
+    private float yCoordinate;
+    private float rssi;
+    private int cnt;
+    private float distance;
+
+    public AccessPoint(String mac, float xCoordinate, float yCoordinate) {
+        this.mac = mac;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public float getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(float xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public float getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(float yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+    public float getRssi() {
+        return rssi;
+    }
+
+    public void setRssi(float rssi) {
+        this.rssi = rssi;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+
+
+}
